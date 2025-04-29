08088ebf2bebcf6dcee714602f0d20913c684557
nguyen van dat <datnv@zolution.vn>
2025-04-29 17:07:56 +0700

    update code

--- babel.config.js
+++ babel.config.js
@@ -7,12 +7,22 @@ module.exports = function (api) {
     ],
     plugins: [
       [
-        ["module:react-native-dotenv"],
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: ".env",
+          blacklist: null,
+          whitelist: null,
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
+      [
         "module-resolver",
         {
           root: ["./src"],
           alias: {
-            "@env": "./.env", 
+            "@": "./src",
           },
         },
       ],
